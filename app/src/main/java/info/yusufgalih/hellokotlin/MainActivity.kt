c061c9e74890cb2186e0d9ced85c5f6589cf6e60
yusufgalih <yusuf.glaih.spensa@gmail.com>
2017-11-21 03:02:25 +0700

    tambah login view

--- app/src/main/java/info/yusufgalih/hellokotlin/MainActivity.kt
+++ app/src/main/java/info/yusufgalih/hellokotlin/MainActivity.kt
@@ -2,11 +2,37 @@ package info.yusufgalih.hellokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to all views
+        var et_user_name = findViewById<EditText>(R.id.et_user_name)
+        var et_password = findViewById<EditText>(R.id.et_password)
+        var btn_reset = findViewById<Button>(R.id.btn_reset)
+        var btn_submit = findViewById<Button>(R.id.btn_submit)
+
+        btn_reset.setOnClickListener {
+            // clearing user_name and password edit text views on reset button click
+            et_user_name.setText("")
+            et_password.setText("")
+        }
+
+        // set on-click listener
+        btn_submit.setOnClickListener {
+            val user_name = et_user_name.text;
+            val password = et_password.text;
+            Toast.makeText(this@MainActivity, user_name, Toast.LENGTH_LONG).show()
+
+            // your code to validate the user_name and password combination
+            // and verify the same
+
+        }
     }
 }
